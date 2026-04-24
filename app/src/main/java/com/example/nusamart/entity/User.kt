3fb9a4862bd13ca0ff8001c5f6f8baa82318ccd3
yasminazzz <khasia.azzahra@gmail.com>
2026-04-24 20:10:04 +0700

    bersihkan comment

--- app/src/main/java/com/example/nusamart/entity/User.kt
+++ app/src/main/java/com/example/nusamart/entity/User.kt
@@ -3,8 +3,8 @@ package com.example.nusamart.entity
 abstract class User(
     open val email: String,
     open val username: String,
-    open val password: String, // Catatan: Sebaiknya jangan simpan plain password di production
-    open val address: String, // Sementara tunggal
-    open val profilePicResId: Int, // Int untuk resource ID drawable/res
+    open val password: String, 
+    open val address: String, 
+    open val profilePicResId: Int, 
     open val role: Boolean
 )
