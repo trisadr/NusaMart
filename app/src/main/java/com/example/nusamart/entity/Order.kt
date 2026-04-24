08e207bacfb5f8d7221347d7d68779a6eb2205e8
yasminazzz <khasia.azzahra@gmail.com>
2026-04-24 20:25:15 +0700

    hapus comment

--- app/src/main/java/com/example/nusamart/entity/Order.kt
+++ app/src/main/java/com/example/nusamart/entity/Order.kt
@@ -11,10 +11,10 @@ enum class OrderStatus {
 data class Order(
     val idOrder: String,
     val totalPrice: Double,
-    val status: OrderStatus, // Menggunakan Enum
+    val status: OrderStatus, 
     val trackingNumber: String,
     val description: String,
-    val orderDate: Long, // Menggunakan Long (Timestamp) atau String
-    val arrivedDate: Long?, // Nullable (?) karena bisa jadi belum sampai
+    val orderDate: Long, 
+    val arrivedDate: Long?, 
     val idSeller: String
 )
