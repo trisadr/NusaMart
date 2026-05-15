f5e21548f5d4d71d418bce1efceb61aa29dcbc32
rifqiamila <rifqiamila@gmail.com>
2026-05-15 07:26:46 +0700

    delete entity lama + tambah isi product item

--- app/src/main/java/com/example/nusamart/entity/OrderItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.nusamart.entity
-
-data class OrderItem(
-    val idOrderItem: String,
-    val idOrder: String,
-    val idProduct: String,
-    val quantity: Int,
-    val priceAtPurchase: Double
-)
